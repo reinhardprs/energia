859ad558766041fe62a9aa29abdd57509e75c892
reinhardprs <reinhardprs@gmail.com>
2024-11-12 11:23:36 +0700

    Add CreatedAt time for user

--- entities/user.go
+++ entities/user.go
@@ -1,9 +1,11 @@
 package entities
 
+import "time"
+
 type User struct {
 	ID        int
 	Email     string
 	Password  string
 	Token     string
-	CreatedAt string
+	CreatedAt time.Time
 }
--- repository/auth/record.go
+++ repository/auth/record.go
@@ -1,25 +1,31 @@
 package auth
 
-import "energia/entities"
+import (
+	"energia/entities"
+	"time"
+)
 
 type User struct {
-	ID       int    `gorm:"primaryKey"`
-	Email    string `gorm:"unique"`
-	Password string `gorm:"not null"`
+	ID        int       `gorm:"primaryKey"`
+	Email     string    `gorm:"unique"`
+	Password  string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
 
 func FromEntities(user entities.User) User {
 	return User{
-		ID:       user.ID,
-		Email:    user.Email,
-		Password: user.Password,
+		ID:        user.ID,
+		Email:     user.Email,
+		Password:  user.Password,
+		CreatedAt: user.CreatedAt,
 	}
 }
 
 func (user User) ToEntities() entities.User {
 	return entities.User{
-		ID:       user.ID,
-		Email:    user.Email,
-		Password: user.Password,
+		ID:        user.ID,
+		Email:     user.Email,
+		Password:  user.Password,
+		CreatedAt: user.CreatedAt,
 	}
 }

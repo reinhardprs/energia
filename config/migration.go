9e4a202787b0d8145b4bf08832128a419b21d363
reinhardprs <reinhardprs@gmail.com>
2024-11-15 14:32:20 +0700

    Penambahan struct untuk migrasi database

--- config/migration.go
+++ config/migration.go
@@ -3,10 +3,17 @@ package config
 import (
 	"energia/repository/auth"
 	"energia/repository/device"
+	device_usage "energia/repository/device-usage"
+	user_usage "energia/repository/user-usage"
 
 	"gorm.io/gorm"
 )
 
 func MigrateDB(db *gorm.DB) {
-	db.AutoMigrate(&auth.User{}, &device.Device{})
+	db.AutoMigrate(
+		&auth.User{},
+		&device.Device{},
+		&device_usage.DeviceUsage{},
+		&user_usage.UserUsage{},
+	)
 }
